6d72762a789be59c768db3609fdea00af89fd914
mengfu188 <1242733702@qq.com>
2019-12-30 10:45:04 +0800

    Pretty output

--- example.py
+++ example.py
@@ -10,6 +10,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--ann-path', default='Annotations')
 parser.add_argument('--clusters', nargs='+', type=int, default=[1, 2, 3, 4, 5, 6, 7, 8, 9])
+parser.add_argument('--img-size', type=int, default=range(320, 609, 32), nargs='+')
 args = parser.parse_args()
 
 ANNOTATIONS_PATH = args.ann_path
@@ -42,6 +43,9 @@ for cluster in CLUSTERS:
     out = kmeans(data, k=cluster)
     print("Accuracy: {:.2f}%".format(avg_iou(data, out) * 100))
     print("Boxes:\n {}".format(out))
+    for size in args.img_size:
+        print(f'for image size {size}, Boxes:')
+        print(out * size)
 
     ratios = np.around(out[:, 0] / out[:, 1], decimals=2).tolist()
     print("Ratios:\n {}".format(sorted(ratios)))
